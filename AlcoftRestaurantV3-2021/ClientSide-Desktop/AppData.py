b48c6c915dce88f6ce1cd604362bc4887a60b3b5
bosdos12 <66145209+bosdos12@users.noreply.github.com>
2022-02-02 22:27:21 +0100

    Delete AppData.py

--- AlcoftRestaurantV3-2021/ClientSide-Desktop/AppData.py
+++
@@ -1,5 +0,0 @@
-ServerUrl = "http://192.168.42.232/"
-ProductDataPath = "./Data/Products/productHolderFolder"
-TVSHVALUEPERCENTAGE = 17
-COLORS = [	"black", "white", "gray",  "silver", "maroon", "red",  "purble", "fushsia", 
-            "green", "lime",  "olive", "yellow", "navy",   "blue", "teal",   "aqua"]
